6d5c05f84031999e593574cbec03da4614cc767e
Elijah Bodden <106613755+Elijah-Bodden@users.noreply.github.com>
2025-11-18 14:54:24 -0500

    Update app.js

--- app.js
+++ app.js
@@ -670,7 +670,7 @@ async function drawPosterCommon(options) {
         ctx.fillText(
           "SONG BY " + artistName.toUpperCase(),
           W - paddingX,
-          imageBottom + gapBelowImage * 0.80
+          imageBottom + gapBelowImage * 0.75
         );
         ctx.fillText(
           durationLabel,
@@ -685,7 +685,7 @@ async function drawPosterCommon(options) {
         ctx.fillText(
           "SONG BY " + artistName.toUpperCase(),
           W - paddingX,
-          imageBottom + gapBelowImage * 0.80
+          imageBottom + gapBelowImage * 0.75
         );
         ctx.fillText(
           durationLabel,
